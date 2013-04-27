ea0464239a51064d31e67d1413912fee1103b32e
catatsuy <catatsuy@catatsuy.org>
2013-04-27 17:41:37 +0900

    auto install
    auto install

---
+++ inits/auto-install.el
@@ -0,0 +1,20 @@
+(require 'cl)
+
+(defvar installing-package-list
+  '(
+    ;; ここに使っているパッケージを書く。
+    php-mode
+    scala-mode
+    markdown-mode
+    scss-mode
+    haskell-mode
+    google-c-style
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+                            when (not (package-installed-p x))
+                            collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+        (package-install pkg))))
