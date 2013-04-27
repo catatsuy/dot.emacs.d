ce9cb94ac6d357b59c4c14e26d7fdeebd9eb95e4
catatsuy <catatsuy@catatsuy.org>
2013-04-27 18:21:47 +0900

    auto install
    auto install

--- inits/20-package.el
+++ inits/20-package.el
@@ -1,3 +1,24 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+;; (require 'cl)
+
+;; (defvar installing-package-list
+;;   '(
+;;     ;; ここに使っているパッケージを書く。
+;;     php-mode
+;;     scala-mode
+;;     markdown-mode
+;;     scss-mode
+;;     haskell-mode
+;;     google-c-style
+;;     ))
+
+;; (let ((not-installed (loop for x in installing-package-list
+;;                             when (not (package-installed-p x))
+;;                             collect x)))
+;;   (when not-installed
+;;     (package-refresh-contents)
+;;     (dolist (pkg not-installed)
+;;         (package-install pkg))))
