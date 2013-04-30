6bd27661f778a7468003442a0a24f542d6834e13
catatsuy <catatsuy@catatsuy.org>
2013-04-30 17:00:58 +0900

    verilog
    verilog

---
+++ inits/30-verilog.el
@@ -0,0 +1,23 @@
+;; Any files that end in .v should be in verilog mode
+(setq auto-mode-alist (cons '("\\.v\\'" . verilog-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.tb\\'" . verilog-mode) auto-mode-alist))
+
+;; Any files in verilog mode shuold have their keywords colorized
+(setq verilog-indent-level             4
+      verilog-indent-level-module      4
+      verilog-indent-level-declaration 4
+      verilog-indent-level-behavioral  4
+      verilog-indent-level-directive   4
+      verilog-case-indent              4
+      verilog-auto-newline             nil
+      verilog-auto-indent-on-newline   nil
+      verilog-tab-always-indent        t
+      verilog-auto-endcomments         t
+      verilog-minimum-comment-distance 40
+      verilog-indent-begin-after-if    t
+      verilog-auto-lineup              '())
+
+(add-hook 'verilog-mode-hook
+        '(lambda ()
+           (add-hook 'local-write-file-hooks
+                   (lambda() (untabify (point-min) (point-max))))))
