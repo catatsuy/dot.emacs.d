fe0558efb9e16afaad4dcf0854442e259f141ec9
catatsuy <catatsuy@catatsuy.org>
2013-05-02 19:41:51 +0900

    miss
    miss

--- inits/00-init.el
+++ inits/00-init.el
@@ -1,7 +1,2 @@
 ;; 起動画面削除
 (setq inhibit-startup-message t)
-
-;; 起動画面で recentf を開く
-(add-hook 'after-init-hook (lambda()
-    (recentf-open-files)
-    ))
--- inits/21-recentf.el
+++ inits/21-recentf.el
@@ -4,3 +4,8 @@
   (setq recentf-auto-cleanup 10)
   (setq recentf-auto-save-timer (run-with-idle-timer 30 t 'recentf-save-list))
   (recentf-mode 1))
+
+;; 起動画面で recentf を開く
+(add-hook 'after-init-hook (lambda()
+    (recentf-open-files)
+    ))
