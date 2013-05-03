e804647331027b5c7c0571c010e307908a40e7d3
catatsuy <catatsuy@catatsuy.org>
2013-05-03 16:30:17 +0900

    recentf
    recentf

--- inits/21-recentf.el
+++ inits/21-recentf.el
@@ -1,9 +1,13 @@
+;; 自動保存
 (when (require 'recentf-ext nil t)
   (setq recentf-max-saved-items 2000)
   (setq recentf-exclude '(".recentf"))
   (setq recentf-auto-cleanup 10)
   (setq recentf-auto-save-timer (run-with-idle-timer 30 t 'recentf-save-list))
   (recentf-mode 1))
+
+
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
 ;; 起動画面で recentf を開く
 (add-hook 'after-init-hook (lambda()
