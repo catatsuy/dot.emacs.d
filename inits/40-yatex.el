035785a9ade734e938d33dac80ba84e5f581ec72
catatsuy <catatsuy@catatsuy.org>
2013-05-30 17:53:10 +0900

    yatex section highlight
    yatex section highlight

--- inits/40-yatex.el
+++ inits/40-yatex.el
@@ -3,9 +3,6 @@
 (setq auto-mode-alist
       (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
 (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
-(setq tex-command "platex")
-(setq dvi2-command "xdvi %s.dvi")
-(setq dviprint-command-format "dvipdfmx %s.dvi")
 (setq YaTeX-use-LaTeX2e t)
 (setq YaTeX-use-AMS-LaTeX t)
 
@@ -13,3 +10,11 @@
 
 ;; 自動改行を切る
 (add-hook ' yatex-mode-hook '(lambda () (auto-fill-mode -1)))
+
+; 野鳥セクションのハイライトの設定．
+; \sectionの色の設定．
+;(setq YaTeX-hilit-sectioning-face '(light時のforecolor/backcolor dark時のforecolor/backcolor))
+(setq YaTeX-hilit-sectioning-face '(darkblue/LightGray LightGray/Black))
+; sectionの階層が変化する時の色の変化の割合（パーセント）
+;(setq YaTeX-hilit-sectioning-attenuation-rate '(light時の割合/dark時の割合))
+(setq YaTeX-hilit-sectioning-attenuation-rate '(0 0))
