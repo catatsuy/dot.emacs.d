a2c45f37df4ba46a5eb23427e5172b749caa2539
catatsuy <catatsuy@catatsuy.org>
2013-06-03 14:49:24 +0900

    vc
    vc

--- inits/10-vc-mode.el
+++ inits/10-vc-mode.el
@@ -1,3 +1,8 @@
 ;; vc-mode 無効
 ;; git とかいじらないし遅い
-(setq vc-handled-backends ())
+;; vcを起動しないようにする
+(custom-set-variables '(vc-handled-backends nil))
+
+;; 不要なhookを外す
+(remove-hook 'find-file-hook 'vc-find-file-hook)
+(remove-hook 'kill-buffer-hook 'vc-kill-buffer-hook)
