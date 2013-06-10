dd876885adfad60af6f86a10a1a416860662b158
catatsuy <catatsuy@catatsuy.org>
2013-06-10 15:00:50 +0900

    ime cursor color
    ime cursor color

--- inits/cocoa-emacs-ime.el
+++ inits/cocoa-emacs-ime.el
@@ -1,9 +1,17 @@
+(setq default-input-method "MacOSX")
+
+(mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `title "あ")
+
+;; カーソルの色
+(mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `cursor-color "red")
+(mac-set-input-method-parameter "com.google.inputmethod.Japanese.Roman" `cursor-color "blue")
+
 ;; emacs 起動時は英数モードから始める
 (add-hook 'after-init-hook 'mac-change-language-to-us)
  
 ;; minibuffer 内は英数モードにする
 (add-hook 'minibuffer-setup-hook 'mac-change-language-to-us)
 (mac-translate-from-yen-to-backslash)
- 
+
 ;; [migemo]isearch のとき IME を英数モードにする
 (add-hook 'isearch-mode-hook 'mac-change-language-to-us)
