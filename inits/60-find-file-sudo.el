f0c942b5d3553c45156b47b4f9ae029528e386fd
catatsuy <catatsuy@catatsuy.org>
2013-06-25 16:09:25 +0900

    add find-file-sudo
    add find-file-sudo

---
+++ inits/60-find-file-sudo.el
@@ -0,0 +1,28 @@
+(defun file-root-p (filename)
+  "Return t if file FILENAME created by root."
+  (eq 0 (nth 2 (file-attributes filename))))
+
+(defun th-rename-tramp-buffer ()
+  (when (file-remote-p (buffer-file-name))
+    (rename-buffer
+     (format "%s:%s"
+             (file-remote-p (buffer-file-name) 'method)
+             (buffer-name)))))
+
+(add-hook 'find-file-hook
+          'th-rename-tramp-buffer)
+
+(defadvice find-file (around th-find-file activate)
+  "Open FILENAME using tramp's sudo method if it's read-only."
+  (if (and (file-root-p (ad-get-arg 0))
+           (not (file-writable-p (ad-get-arg 0)))
+           (y-or-n-p (concat "File "
+                             (ad-get-arg 0)
+                             " is read-only.  Open it as root? ")))
+      (th-find-file-sudo (ad-get-arg 0))
+    ad-do-it))
+
+(defun th-find-file-sudo (file)
+  "Opens FILE with root privileges."
+  (interactive "F")
+  (set-buffer (find-file (concat "/sudo::" file))))
