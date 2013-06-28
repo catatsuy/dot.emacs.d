69a04770d66298d5d0206183a6d68e0d35126039
catatsuy <catatsuy@catatsuy.org>
2013-06-28 14:50:24 +0900

    window-split
    window-split

--- inits/00-keybind.el
+++ inits/00-keybind.el
@@ -7,11 +7,6 @@
 ;; 行番号を指定して移動する機能をM-zに割り当て
 (global-set-key "\M-z" 'goto-line)
 
-;; 次のウィンドウへ移動
-(define-key global-map (kbd "C-M-n") 'next-multiframe-window)
-;; 前のウィンドウへ移動
-(define-key global-map (kbd "C-M-p") 'previous-multiframe-window)
-
 ;; to LARGE C-x C-u と to small C-x C-l
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
