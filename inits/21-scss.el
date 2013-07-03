631d7851208a7615e82317c2752030810f816af5
catatsuy <catatsuy@catatsuy.org>
2013-07-03 14:14:41 +0900

    scss
    scss

--- inits/21-scss.el
+++ inits/21-scss.el
@@ -1,6 +1,13 @@
 (autoload 'scss-mode "scss-mode")
-(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(scss\\|css\\)\\'" . scss-mode))
 
- ;; 自動コンパイルをオフにする
+(defun scss-custom ()
+  "scss-mode-hook"
+  (set (make-local-variable 'css-indent-offset) 2)
+  )
+(add-hook 'scss-mode-hook
+  '(lambda() (scss-custom)))
+
+;; 自動コンパイルをオフにする
 (setq scss-compile-at-save nil)
 
