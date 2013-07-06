183d9272e74f5f664248937cb4106bdb6fa1e21d
catatsuy <catatsuy@catatsuy.org>
2013-07-06 14:24:01 +0900

    modify
    modify

--- inits/22-google-translate.el
+++ inits/22-google-translate.el
@@ -1,11 +1,12 @@
 (require 'google-translate)
 
 (global-set-key "\C-xt" 'google-translate-at-point)
+(global-set-key "\C-xT" 'google-translate-query-translate)
 
-;; 翻訳のデフォルト値を設定（ja -> en）
+;; 翻訳のデフォルト値を設定(ja -> en)（無効化は C-u する）
 (custom-set-variables
-  '(google-translate-default-source-language "ja")
-  '(google-translate-default-target-language "en"))
+ '(google-translate-default-source-language "ja")
+ '(google-translate-default-target-language "en"))
 
 ;; google-translate.elの翻訳バッファをポップアップで表示させる
 (push '("*Google Translate*") popwin:special-display-config)
