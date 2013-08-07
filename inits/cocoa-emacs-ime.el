a90e8fc4c5efcfa067184a8cc954dae683cb8118
catatsuy <catatsuy@catatsuy.org>
2013-08-07 16:09:56 +0900

    add comment
    add comment

--- inits/cocoa-emacs-ime.el
+++ inits/cocoa-emacs-ime.el
@@ -9,9 +9,11 @@
 ;; emacs 起動時は英数モードから始める
 (add-hook 'after-init-hook 'mac-change-language-to-us)
  
+;; backslash を優先
+(mac-translate-from-yen-to-backslash)
+
 ;; minibuffer 内は英数モードにする
 (add-hook 'minibuffer-setup-hook 'mac-change-language-to-us)
-(mac-translate-from-yen-to-backslash)
 
 ;; [migemo]isearch のとき IME を英数モードにする
 (add-hook 'isearch-mode-hook 'mac-change-language-to-us)
