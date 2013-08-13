5456ddfdc2cfeb2fafebcfd40fd9c9fc5713b037
catatsuy <catatsuy@catatsuy.org>
2013-08-13 17:54:51 +0900

    pretty lambda
    pretty lambda

---
+++ inits/60-pretty-lambda.el
@@ -0,0 +1,8 @@
+(defun pretty-lambdas ()
+  (font-lock-add-keywords
+   nil `(("(\\(lambda\\>\\)"
+          (0 (progn (compose-region (match-beginning 1) (match-end 1)
+                                    ,(make-char 'greek-iso8859-7 107))
+                    nil))))))
+
+(add-hook 'emacs-lisp-mode-hook 'pretty-lambdas)
