2288a63f0ed5858dbdb390c4df8d7d5ad9d59e2b
catatsuy <catatsuy@catatsuy.org>
2013-08-20 23:41:05 +0900

    add js setting in ixiv project
    add js setting in ixiv project
    http://qiita.com/tadsan/items/6aea9d6143f722900260

--- inits/21-js2.el
+++ inits/21-js2.el
@@ -1,1 +1,8 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+;; Settings in certain project
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (when (string-match ".*ixiv\\.ne.*/.*\\.js"
+                                (or (buffer-file-name) ""))
+              (setq tab-width 2 indent-tabs-mode t))))
