d3b189547c336ecda08327fa4561eab3f121cb7a
catatsuy <catatsuy@catatsuy.org>
2013-08-21 00:17:49 +0900

    case-sensitive
    case-sensitive in ruby-mode

--- inits/30-ruby.el
+++ inits/30-ruby.el
@@ -4,3 +4,10 @@
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
+;; auto-complete環境で補完の大文字小文字を区別する
+;; cf: http://qiita.com/tadsan/items/ab3c3b594b5bf6203f02
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (make-local-variable 'ac-ignore-case)
+            (setq ac-ignore-case nil)))
