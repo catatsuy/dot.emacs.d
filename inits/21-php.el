8d8e965e4a70aca9e0912c97caaad899c8502bf8
catatsuy <catatsuy@catatsuy.org>
2013-08-26 17:00:50 +0900

    The php-mode indentation for array is not good
    The php-mode indentation for array is not good
    cf: http://www.emacswiki.org/emacs/PhpMode#toc16

--- inits/21-php.el
+++ inits/21-php.el
@@ -2,3 +2,17 @@
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
 (setq auto-mode-alist (cons '("\\.ctp$" . html-mode) auto-mode-alist))
+
+;; Indentation of arrays
+;; cf: http://www.emacswiki.org/emacs/PhpMode#toc16
+(add-hook 'php-mode-hook (lambda ()
+    (defun ywb-php-lineup-arglist-intro (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (+ (current-column) c-basic-offset))))
+    (defun ywb-php-lineup-arglist-close (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (current-column))))
+    (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+    (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
