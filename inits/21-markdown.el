285b0ccd20d417a93ac39f4ef51fe927e080103f
catatsuy <catatsuy@catatsuy.org>
2013-10-05 20:33:39 +0900

    gfm-mode
    gfm-mode

--- inits/21-markdown.el
+++ inits/21-markdown.el
@@ -1,2 +1,2 @@
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
-(setq auto-mode-alist (cons '("\\.md$" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.md$" . gfm-mode) auto-mode-alist))
