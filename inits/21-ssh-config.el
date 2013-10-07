0377a41efeaaddc8641b693c443d5ade8e379d69
catatsuy <catatsuy@catatsuy.org>
2013-10-07 12:31:02 +0900

    add ssh-config

--- inits/21-ssh-config.el
+++ inits/21-ssh-config.el
@@ -1,4 +1,5 @@
 (autoload 'ssh-config-mode "ssh-config-mode" t)
 (add-to-list 'auto-mode-alist '(".ssh/config\\'"  . ssh-config-mode))
 (add-to-list 'auto-mode-alist '("sshd?_config\\'" . ssh-config-mode))
+(add-to-list 'auto-mode-alist '("sshd?-config" . ssh-config-mode))
 (add-hook 'ssh-config-mode-hook 'turn-on-font-lock)
