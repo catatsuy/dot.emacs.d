9b32d93c7855deb97e51db40ae9561c97b58e3b4
catatsuy <catatsuy@catatsuy.org>
2013-10-12 20:50:35 +0900

    slow down scroll speed
    slow down scroll speed

---
+++ inits/cocoa-emacs-scroll.el
@@ -0,0 +1,13 @@
+;; ホイールスクロールを控えめに
+;; cf: http://tjun.org/blog/2009/11/emacs_mouse_whee/
+(defun scroll-down-with-lines ()
+  "" (interactive) (scroll-down 3))
+(defun scroll-up-with-lines ()
+  "" (interactive) (scroll-up 3))
+
+(global-set-key [wheel-up] 'scroll-down-with-lines)
+(global-set-key [wheel-down] 'scroll-up-with-lines)
+(global-set-key [double-wheel-up] 'scroll-down-with-lines)
+(global-set-key [double-wheel-down] 'scroll-up-with-lines)
+(global-set-key [triple-wheel-up] 'scroll-down-with-lines)
+(global-set-key [triple-wheel-down] 'scroll-up-with-lines)
