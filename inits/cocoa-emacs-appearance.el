f867122ecb7db148531d0071e5e8d84282658301
catatsuy <catatsuy@catatsuy.org>
2013-10-12 20:51:41 +0900

    font size 11->12
    font size 11->12

--- inits/cocoa-emacs-appearance.el
+++ inits/cocoa-emacs-appearance.el
@@ -1,5 +1,5 @@
-(set-default-font "Bitstream Vera Sans Mono-11")
-(set-face-font 'variable-pitch "Bitstream Vera Sans Mono-11")
+(set-default-font "Bitstream Vera Sans Mono-12")
+(set-face-font 'variable-pitch "Bitstream Vera Sans Mono-12")
 (set-fontset-font (frame-parameter nil 'font)
                   'japanese-jisx0208
                   '("ヒラギノ角ゴ Pro W3" . "unicode-bmp")
