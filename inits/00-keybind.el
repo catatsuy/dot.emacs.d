2e280a388aaa9e7fa8bcbf931c2752606c3a4afa
catatsuy <catatsuy@catatsuy.org>
2013-11-07 18:30:59 +0900

    esc esc
    esc esc
    cf: http://qiita.com/tadsan/items/e7557c8db33a3c590fec

--- inits/00-keybind.el
+++ inits/00-keybind.el
@@ -4,6 +4,9 @@
 ;; 行番号を指定して移動する機能をM-zに割り当て
 (global-set-key "\M-z" 'goto-line)
 
+;; ESC 連打で無効化
+(global-set-key (kbd "M-ESC ESC") 'keyboard-quit)
+
 ;; to LARGE C-x C-u と to small C-x C-l
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
