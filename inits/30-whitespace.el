3df51cc21b675c603aeaa1b535698ea2f96a789b
catatsuy <catatsuy@catatsuy.org>
2013-11-10 18:06:50 +0900

    display full-width-space

--- inits/30-whitespace.el
+++ inits/30-whitespace.el
@@ -1,3 +1,9 @@
 (require 'whitespace)
-(setq whitespace-style '(face tabs tab-mark lines-tail trailing space-before-tab space-after-tab::space))
+
+(setq whitespace-style '(face tabs tab-mark spaces space-mark lines-tail trailing space-before-tab space-after-tab::space))
+(setq whitespace-space-regexp "\\(\x3000+\\)")
+(setq whitespace-display-mappings
+      '((space-mark ?\x3000 [?\□])
+        (tab-mark   ?\t   [?\xBB ?\t])
+        ))
 (global-whitespace-mode t)
