b9a179263502c2c3c4c46dd21d51d236c5aa9a68
catatsuy <catatsuy@catatsuy.org>
2013-12-31 12:35:56 +0900

    リージョンを isearch する
    cf: http://shibayu36.hatenablog.com/entry/2013/12/30/190354

---
+++ inits/60-region-isearch.el
@@ -0,0 +1,12 @@
+;;; 選択範囲をisearch
+(defadvice isearch-mode (around isearch-mode-default-string (forward &optional regexp op-fun recursive-edit word-p) activate)
+  (if (and transient-mark-mode mark-active (not (eq (mark) (point))))
+      (progn
+        (isearch-update-ring (buffer-substring-no-properties (mark) (point)))
+        (deactivate-mark)
+        ad-do-it
+        (if (not forward)
+            (isearch-repeat-backward)
+          (goto-char (mark))
+          (isearch-repeat-forward)))
+    ad-do-it))
