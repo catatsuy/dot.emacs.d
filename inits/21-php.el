2e118b02ab4cd0696fc91e13476eed40f69d29cd
catatsuy <catatsuy@catatsuy.org>
2014-04-20 21:36:08 +0900

    use pear

--- inits/21-php.el
+++ inits/21-php.el
@@ -3,12 +3,6 @@
 (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
 (setq auto-mode-alist (cons '("\\.ctp$" . html-mode) auto-mode-alist))
 
-(setq php-mode-force-pear t)
-
-;; http://d.hatena.ne.jp/gct/20101126/1290755504
 (add-hook 'php-mode-hook
           (lambda ()
-            (c-set-offset 'case-label' 4)
-            (c-set-offset 'arglist-intro' 4)
-            (c-set-offset 'arglist-cont-nonempty' 4)
-            (c-set-offset 'arglist-close' 0)))
+            (c-set-style "pear")))
