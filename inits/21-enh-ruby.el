a91bf888a685fbacd8570eea102a2c662d08bb4c
catatsuy <catatsuy@catatsuy.org>
2014-05-08 19:08:04 +0900

    *.cap, config.ru in enh-ruby

--- inits/21-enh-ruby.el
+++ inits/21-enh-ruby.el
@@ -2,6 +2,8 @@
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.cap$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("config.ru$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
