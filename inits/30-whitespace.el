4e4c6af510730ce46826b63eecfe5d0d095706d2
catatsuy <catatsuy@catatsuy.org>
2014-06-05 22:40:19 +0900

    rm tabs

--- inits/30-whitespace.el
+++ inits/30-whitespace.el
@@ -1,6 +1,6 @@
 (require 'whitespace)
 
-(setq whitespace-style '(face tabs tab-mark spaces space-mark lines-tail trailing space-before-tab space-after-tab::space))
+(setq whitespace-style '(face tab-mark spaces space-mark lines-tail trailing space-before-tab space-after-tab::space))
 (setq whitespace-space-regexp "\\(\x3000+\\)")
 (setq whitespace-display-mappings
       '((space-mark ?\x3000 [?\□])
