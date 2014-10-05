3edb81db8a042c51128dae9c1e3a522b36702b28
catatsuy <catatsuy@catatsuy.org>
2014-10-05 16:30:50 +0900

    必要なくなった

--- inits/30-shell-script.el
+++ inits/30-shell-script.el
@@ -1,6 +1,3 @@
 ;; indent
 (add-hook 'sh-mode-hook '(lambda () (interactive)
                            (setq sh-basic-offset 2 sh-indentation 2)))
-
-;; .zshrc.* を shell-script-mode に
-(add-to-list 'auto-mode-alist '("\\.zshrc" . shell-script-mode))
