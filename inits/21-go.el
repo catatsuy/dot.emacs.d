7e99e10934ac5f6c27f3352b61753cc360bed882
catatsuy <catatsuy@catatsuy.org>
2014-10-07 20:38:32 +0900

    不要なGoのキーバインド削除

--- inits/21-go.el
+++ inits/21-go.el
@@ -6,8 +6,6 @@
              (setq c-basic-offset 4)
              (setq tab-width 2 indent-tabs-mode t)
              (local-set-key (kbd "M-.") 'godef-jump)
-             (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
-             (local-set-key (kbd "C-c i") 'go-goto-imports)
-             (local-set-key (kbd "C-c d") 'godoc)))
+             ))
 
 (add-hook 'before-save-hook 'gofmt-before-save)
