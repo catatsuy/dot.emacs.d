8c9b79156fd4940cc83b160b299197926a859dfc
catatsuy <catatsuy@catatsuy.org>
2014-10-26 00:02:08 +0900

    bug workaround

--- inits/21-popwin.el
+++ inits/21-popwin.el
@@ -3,4 +3,7 @@
 (setq display-buffer-function 'popwin:display-buffer)
 (setq popwin:popup-window-position 'bottom)
 
+;; cf: https://github.com/emacs-jp/issues/issues/2
+(setq popwin:close-popup-window-timer-interval 0.5)
+
 (push '("*Occur*") popwin:special-display-config)
