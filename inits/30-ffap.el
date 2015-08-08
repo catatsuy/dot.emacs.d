575a1b48592cd9199bc049876f5142a1bacf5532
catatsuy <catatsuy@catatsuy.org>
2015-08-08 17:39:03 +0900

    rm ffap

--- inits/30-ffap.el
+++
@@ -1,9 +0,0 @@
-;; http://rubikitch.com/2015/07/16/ffap-dired/
-;;; ファイル名補完キーマップで?をそのまま入力できるようにする
-(define-key minibuffer-local-filename-completion-map (kbd "?") nil)
-
-;;; ffapでワイルドカードを指定するとdiredを開くようにする
-(setq ffap-pass-wildcards-to-dired t)
-
-;;; C-x C-fなどをffap関係のコマンドに割り当てる
-(ffap-bindings)
