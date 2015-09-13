3fcd338cd6e498758efb334ba8d13cfe10fc6731
catatsuy <catatsuy@catatsuy.org>
2015-09-13 18:53:15 +0900

    ATOK対応

--- inits/cocoa-emacs-ime.el
+++ inits/cocoa-emacs-ime.el
@@ -1,9 +1,11 @@
 (setq default-input-method "MacOSX")
 
 (mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `title "あ")
+(mac-set-input-method-parameter "com.justsystems.inputmethod.atok28.Japanese" `title "あ")
 
 ;; カーソルの色
 (mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `cursor-color "red")
+(mac-set-input-method-parameter "com.justsystems.inputmethod.atok28.Japanese" `cursor-color "red")
 (mac-set-input-method-parameter "com.apple.keylayout.US" `cursor-color "blue")
 ;; (mac-set-input-method-parameter "com.google.inputmethod.Japanese.Roman" `cursor-color "blue")
 
