6b2f3629269fc248c009abecbe7a9ecd74c0931f
catatsuy <catatsuy@catatsuy.org>
2016-05-15 18:58:58 +0900

    font

--- inits/cocoa-emacs-appearance.el
+++ inits/cocoa-emacs-appearance.el
@@ -1,6 +1,10 @@
 (set-default-font "Bitstream Vera Sans Mono-12")
 (set-face-font 'variable-pitch "Bitstream Vera Sans Mono-12")
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0208
-                  '("ヒラギノ角ゴ Pro W3" . "unicode-bmp")
-                  )
+(set-fontset-font
+ (frame-parameter nil 'font)
+ 'japanese-jisx0208
+ '("Hiragino Kaku Gothic ProN" . "unicode-bmp"))
+(set-fontset-font
+ (frame-parameter nil 'font)
+ 'japanese-jisx0212
+ '("Hiragino Kaku Gothic ProN" . "unicode-bmp"))
