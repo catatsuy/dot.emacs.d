c81632884f0c1dad43686c50cc66339bb9082f76
catatsuy <catatsuy@catatsuy.org>
2017-08-12 20:15:07 +0900

    remove

--- inits/cocoa-emacs-markdown.el
+++
@@ -1,9 +0,0 @@
-(defun markdown-preview-file ()
-  "run Marked on the current file and revert the buffer"
-  (interactive)
-  (shell-command
-   (format "open -a '/Applications/Marked 2.app' %s"
-           (shell-quote-argument (buffer-file-name))))
-  )
-
-(global-set-key (kbd "C-c m") 'markdown-preview-file)
